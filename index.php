53b2a4e49432edc055aeadc8269a69cf494a384f
ArrowX17 <iamsamix17@gmail.com>
2019-07-09 22:15:20 +0600

    v0.0.0.02 _topnav_homeSlider

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>houseBuy</title>
   <link rel="stylesheet" href="css/bootstrap.css">
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
   <nav class="navbar navbar-expand-lg navbar-light fixed-top"> <!-- top navbar fixed [START] -->
     <div class="container">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="#">HouseBuyLogo</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
       </button>
@@ -34,7 +34,7 @@
     </div>
   </nav> <!-- top navbar fixed [END] -->
 
-  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel"> <!-- top img slider [START] -->
     <ol class="carousel-indicators">
       <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
       <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
@@ -45,7 +45,7 @@
         <img src="img/hs01.jpg" class="d-block w-100" alt="...">
       </div>
       <div class="carousel-item">
-        <img src="img/hs02.jpg" class="d-block w-100" alt="...">
+        <img src="img/hs01.jpg" class="d-block w-100" alt="...">
       </div>
       <div class="carousel-item">
         <img src="img/hs01.jpg" class="d-block w-100" alt="...">
@@ -59,7 +59,7 @@
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="sr-only">Next</span>
     </a>
-  </div>
+  </div> <!-- top img slider [END] -->
 
 <script src="js/jquery-3.4.1.min.js"></script>
 <script src="js/popper.min.js"></script>
