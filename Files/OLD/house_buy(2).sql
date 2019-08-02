ce4dfa828f7bdb371a3a583565189182e42dd346
ArrowX17 <iamsamix17@gmail.com>
2019-08-03 04:10:31 +0600

    db as of 8.3.19

--- Files/OLD/house_buy(2).sql
+++ Files/OLD/house_buy(2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Aug 02, 2019 at 10:08 PM
+-- Generation Time: Aug 01, 2019 at 10:55 AM
 -- Server version: 5.7.26
 -- PHP Version: 7.2.18
 
@@ -52,7 +52,6 @@ CREATE TABLE IF NOT EXISTS `employee` (
   `EMP_ID` int(11) NOT NULL AUTO_INCREMENT,
   `USERNAME` varchar(10) NOT NULL,
   `EMAIL` varchar(50) NOT NULL,
-  `type` varchar(20) NOT NULL,
   `Password` varchar(255) NOT NULL,
   `Name` varchar(50) NOT NULL,
   `Address` varchar(200) NOT NULL,
@@ -65,8 +64,8 @@ CREATE TABLE IF NOT EXISTS `employee` (
 -- Dumping data for table `employee`
 --
 
-INSERT INTO `employee` (`EMP_ID`, `USERNAME`, `EMAIL`, `type`, `Password`, `Name`, `Address`) VALUES
-(10002, 'ArrowX17', 'iamsamix17@gmail.com', 'Admin', 'aaaaaaaa', 'Atique Morshed', '69 Mohakhali, Dhaka');
+INSERT INTO `employee` (`EMP_ID`, `USERNAME`, `EMAIL`, `Password`, `Name`, `Address`) VALUES
+(10002, 'ArrowX17', 'iamsamix17@gmail.com', 'aaaaaaaaaa', 'Atique Morshed', '69 Mohakhali, Dhaka');
 
 -- --------------------------------------------------------
 
@@ -95,7 +94,7 @@ DROP TABLE IF EXISTS `preproperty`;
 CREATE TABLE IF NOT EXISTS `preproperty` (
   `PRE_ID` int(11) NOT NULL AUTO_INCREMENT,
   `USERNAME` varchar(10) NOT NULL,
-  `verify` int(11) NOT NULL,
+  `EMP_ID` int(11) NOT NULL,
   `bedroom` int(11) NOT NULL,
   `washroom` int(11) NOT NULL,
   `balcony` int(11) NOT NULL,
@@ -103,19 +102,10 @@ CREATE TABLE IF NOT EXISTS `preproperty` (
   `street` varchar(200) NOT NULL,
   `city` varchar(50) NOT NULL,
   `state` varchar(50) NOT NULL,
-  `Description` varchar(2000) NOT NULL,
   PRIMARY KEY (`PRE_ID`,`USERNAME`),
-  KEY `USERNAME` (`USERNAME`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `preproperty`
---
-
-INSERT INTO `preproperty` (`PRE_ID`, `USERNAME`, `verify`, `bedroom`, `washroom`, `balcony`, `size`, `street`, `city`, `state`, `Description`) VALUES
-(4, 'ArrowX17', 0, 2, 2, 2, 1, 'wada', 'dd', 'dd', 'wdswadwdadw'),
-(10, 'abudada', 0, 2, 3, 4, 1, 'dd', 'Dhaka', 'Dhaka', 'aaaa'),
-(11, 'ArrowX17', 0, 1, 2, 3, 6, 'Sadat', 'Sadat', 'Sadat', 'Sadat');
+  KEY `USERNAME` (`USERNAME`),
+  KEY `EMP_ID` (`EMP_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -127,20 +117,15 @@ DROP TABLE IF EXISTS `property`;
 CREATE TABLE IF NOT EXISTS `property` (
   `PROPERTY_ID` int(11) NOT NULL AUTO_INCREMENT,
   `EMP_ID` int(11) NOT NULL,
-  `USERNAME` varchar(10) NOT NULL,
   `PROPERTY_NAME` varchar(100) NOT NULL,
   `PRICE` int(11) NOT NULL,
   `BEDROOM` int(11) NOT NULL,
   `WASHROOM` int(11) NOT NULL,
   `BALCONY` int(11) DEFAULT NULL,
   `SIZE` int(11) NOT NULL,
-  `street` varchar(200) NOT NULL,
-  `city` varchar(20) NOT NULL,
-  `state` varchar(20) NOT NULL,
-  `Description` varchar(2000) NOT NULL,
+  `LOCATION` varchar(200) NOT NULL,
   PRIMARY KEY (`PROPERTY_ID`),
-  KEY `EMP_ID` (`EMP_ID`),
-  KEY `USERNAME` (`USERNAME`)
+  KEY `EMP_ID` (`EMP_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -156,18 +141,11 @@ CREATE TABLE IF NOT EXISTS `user` (
   `EMAIL` varchar(50) NOT NULL,
   `Name` varchar(50) NOT NULL,
   `ADDRESS` varchar(200) NOT NULL,
+  `PHONE` int(10) NOT NULL,
   PRIMARY KEY (`USERNAME`),
-  UNIQUE KEY `EMAIL` (`EMAIL`)
+  UNIQUE KEY `EMAIL` (`EMAIL`),
+  UNIQUE KEY `PHONE` (`PHONE`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`USERNAME`, `Password`, `EMAIL`, `Name`, `ADDRESS`) VALUES
-('abudada', '$2y$10$oIUN2l.QhgQmom.wa3QkXOI371wR13hOfNq2DBNVlx0K0mdIWXjKi', 'abu@sadat.com', 'Sadat', 'Dhaka'),
-('ArrowX17', '$2y$10$TISDyaGGqbijR/QwMqofx.XwVi8goR1BE/QFP24RdM7j2qj/.YCX.', 'iamsamix17@gmail.com', 'Atique Morshed Sami', 'Dhaka'),
-('Rahimm', '$2y$10$CG5K4wAJ4dkYhTR4cJgMWe6365f9b8w4/UoM4cA1O7iXyJy2eJlFy', 'r@r.com', 'Rahim', 'AAA');
 
 --
 -- Constraints for dumped tables
@@ -191,14 +169,14 @@ ALTER TABLE `payment`
 -- Constraints for table `preproperty`
 --
 ALTER TABLE `preproperty`
-  ADD CONSTRAINT `preproperty_ibfk_1` FOREIGN KEY (`USERNAME`) REFERENCES `user` (`USERNAME`);
+  ADD CONSTRAINT `preproperty_ibfk_1` FOREIGN KEY (`USERNAME`) REFERENCES `user` (`USERNAME`),
+  ADD CONSTRAINT `preproperty_ibfk_2` FOREIGN KEY (`EMP_ID`) REFERENCES `employee` (`EMP_ID`);
 
 --
 -- Constraints for table `property`
 --
 ALTER TABLE `property`
-  ADD CONSTRAINT `property_ibfk_1` FOREIGN KEY (`EMP_ID`) REFERENCES `employee` (`EMP_ID`),
-  ADD CONSTRAINT `property_ibfk_2` FOREIGN KEY (`USERNAME`) REFERENCES `user` (`USERNAME`);
+  ADD CONSTRAINT `property_ibfk_1` FOREIGN KEY (`EMP_ID`) REFERENCES `employee` (`EMP_ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
